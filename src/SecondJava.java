d2c5970fe07c191fa0d38a2bce34f64bda7f5d76
Yoon <wuis749@naver.com>
2020-09-05 14:10:32 +0900

    첫번째 commit

---
+++ src/SecondJava.java
@@ -0,0 +1,13 @@
+
+public class SecondJava {
+public static void main(String[] args) {
+	
+	int a =5 ;
+	double b = 5.5;
+	
+	double c = a+b;
+	
+	System.out.println(c); 
+	
+}
+}
---
+++ src/StartJava.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class StartJava {
+
+	public static void main(String[] args) {
+		
+		Scanner scanner= new Scanner(System.in);
+		int a= 10 ;
+		String name ;
+		
+		System.out.println("이름은 뭐니 ");
+		name= scanner.next();
+		System.out.println("너의 이름은 "+name +"나이는 " +  a) ;
+		 
+	}
+}
